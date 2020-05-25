01c4ec0ab48636f543c2312d376536e56740d676
Dobsgw <38656887+Dobsgw@users.noreply.github.com>
2020-05-25 11:13:03 +0000

    tissot: Add GCam GO

--- device.mk
+++ device.mk
@@ -72,6 +72,10 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
+# GCam Go
+PRODUCT_PACKAGES += \
+    GCam
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
