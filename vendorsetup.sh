c457b4c5691dfd410ff90765106419e686e15590
Unknownbitch <68426327+Unknownbitch07@users.noreply.github.com>
2021-09-06 14:21:27 +0530

    tissot: add vendorsetup script

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+rm -rf hardware/qcom-caf/msm8996/display
+rm -rf hardware/qcom-caf/msm8996/media
+git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_display.git -b eleven hardware/qcom-caf/msm8996/display
+git clone  https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_media.git -b eleven hardware/qcom-caf/msm8996/media
