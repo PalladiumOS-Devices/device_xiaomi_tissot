2f1b8f9aefa72c95d92f92f367aa2ae48819b87e
Sanjheevi <sanjheevisv@palladiumos.com>
2022-03-21 00:31:43 +0530

    tissot: add maintainer prop

--- palladium_tissot.mk
+++ palladium_tissot.mk
@@ -41,10 +41,15 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "google/raven/raven:12/S3B1.220218.004/8242181:user/release-keys"
 
-# PixelExperience Stuffs
+# PalladiumOS Stuffs
+PALLADIUM_BUILD_TYPE := OFFICIAL
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.palladium.maintainer=ucoboy
+
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_STOCK_ARCORE := false
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_SUPPORTS_QUICK_TAP := true
+
